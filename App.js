dfbb158f03666a0b2a841c55f5f20fc25c7a8635
dannyr22 <dannyr2763@hotmail.com>
2021-02-01 21:57:47 +0000

    intial JSX template for first page created

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View,} from 'react-native';
+import { FlatList, StyleSheet, Text, View,} from 'react-native';
 
 export default function App() {
-
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
  
-
   return (
     <View style={styles.container}>
-    
+      {/* header */}
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+           />
+        </View>
       </View>
+    </View>
   );
 }
 
@@ -17,5 +31,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF',
-  }
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
 });
