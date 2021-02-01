57a2fbd5f97d9407b0b73fce0238e3e1387c5264
dannyr22 <dannyr2763@hotmail.com>
2021-02-01 22:17:15 +0000

    Header component initialised - still needs a lot more styling

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { FlatList, StyleSheet, Text, View,} from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -12,6 +13,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* header */}
+      <Header />
       <View style={styles.content}>
         {/* to form */}
         <View style={styles.list}>
